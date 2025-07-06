b4fb8b21b39734fd36b694809cdbe55a0d410611
IrynaKr1 <krutsiryna@gmail.com>
2025-07-06 12:54:04 +0200

    Add argument to function

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,14 @@
-function customCounter() {
-  const iteration = Number(prompt('Enter start value: '));
-  const step = Number(prompt('Enter step value: '));
+const iteration = Number(prompt('Enter start value: '));
+const step = Number(prompt('Enter step value: '));
 
+function customCounter(iteration, step) {
   let i = iteration;
   return function () {
     return (i += step);
   };
 }
 
-const counter1 = customCounter();
+const counter1 = customCounter(iteration, step);
 
 console.log(counter1());
 console.log(counter1());
